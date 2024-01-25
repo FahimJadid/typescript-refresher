6e60a6f645b3a7fc23b44b9a2756667d05429302
Fahim Al Jadid <b170606023@pharm.jnu.ac.bd>
2024-01-25 16:42:10 +0600

    initialized typescript

--- app.js
+++ app.js
@@ -1,32 +1,6 @@
-// let character = "Jadid";
-// let age = 30;
-// let isBlackBelt = false;
-// // character = 20
-// character = "Jadid";
-// let names: string = "jadid";
-// let isMarried: boolean = false;
-// let value: string | number;
-// value = 10;
-// value = "10";
-// let assassins: string[] = ["jadid", "al", "fahim"];
-// const circ = (diameter: number) => {
-//   return diameter * Math.floor(Math.PI);
-// };
-// console.log(circ(5.5));
-// let names: string[] = ["jadid", "al", "fahim"];
-// names.push("moon");
-// names[0] = "lamia";
-// console.log(names);
-// let numbers: number[] = [1, 2, 3, 4, 5];
-// numbers.push(6);
-// console.log(numbers);
-// let names = ["jadid", 1, "al", 2, 3, 4, 5];
-// console.log(names);
-// let arr: Array<number> = [1, 2, 3, 4, 5];
-// console.log(arr);
-// let object = {
-//   name: "jadid",
-//   age: 30,
-// };
-// object.age = 40;
-// object.name = "al";
+var num1 = 10;
+var num2 = 20;
+function sum(a, b) {
+    return a + b;
+}
+sum(num1, num2);
--- app.ts
+++ app.ts
@@ -1,51 +1,8 @@
-// let character = "Jadid";
+const num1 = 10;
+const num2 = 20;
 
-// let age = 30;
+function sum(a: number, b: number) {
+  return a + b;
+}
 
-// let isBlackBelt = false;
-
-// // character = 20
-
-// character = "Jadid";
-
-// let names: string = "jadid";
-
-// let isMarried: boolean = false;
-
-// let value: string | number;
-// value = 10;
-// value = "10";
-
-// let assassins: string[] = ["jadid", "al", "fahim"];
-
-// const circ = (diameter: number) => {
-//   return diameter * Math.floor(Math.PI);
-// };
-
-// console.log(circ(5.5));
-
-// let names: string[] = ["jadid", "al", "fahim"];
-
-// names.push("moon");
-// names[0] = "lamia";
-// console.log(names);
-
-// let numbers: number[] = [1, 2, 3, 4, 5];
-
-// numbers.push(6);
-
-// console.log(numbers);
-
-// let names = ["jadid", 1, "al", 2, 3, 4, 5];
-// console.log(names);
-// let arr: Array<number> = [1, 2, 3, 4, 5];
-
-// console.log(arr);
-
-// let object = {
-//   name: "jadid",
-//   age: 30,
-// };
-
-// object.age = 40;
-// object.name = "al";
+sum(num1, num2);
