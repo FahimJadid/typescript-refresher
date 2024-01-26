f6b5ca8b5ad3b0d53cf8fc6d97e1a029c311b86d
Fahim Al Jadid <b170606023@pharm.jnu.ac.bd>
2024-01-26 10:06:10 +0600

    rest params

--- Bundler-init-103/src/main.ts
+++ Bundler-init-103/src/main.ts
@@ -1,3 +1,6 @@
-const data = fetch("https://jsonplaceholder.typicode.com/todos/1")
-  .then((response) => response.json())
-  .then((result) => console.log(result));
+function sum(...nums: number[]) {
+  return nums.reduce((a, b) => a + b, 0);
+}
+
+sum(1, 2, 3, 4, 5);
+sum(1, 2);
